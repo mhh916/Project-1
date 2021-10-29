df584af634be548b7fdba4a284b8cae37bd2c0fd
Michael Hayes <michaelhayes22222@gmail.com>
2021-10-29 17:30:25 -0400

    Setup basic API functionallity

--- src/main/scala/helpers/apiConnect.scala
+++ src/main/scala/helpers/apiConnect.scala
@@ -1,11 +1,18 @@
 package helpers
 
+
 import org.apache.http.HttpEntity
 import org.apache.http.HttpResponse
 import org.apache.http.client.ClientProtocolException
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.DefaultHttpClient
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import java.io.File;
+import java.io.PrintWriter;
 
 class apiConnect() {
     def searchEverything(search: String, sources: String, from: String, to: String, language: String, sortBy: String, page: String): Unit = {
@@ -20,11 +27,11 @@ class apiConnect() {
                   "&page=" + page +
                   "&apiKey=0425fffcba054e00a1e722ffb03f7ff5"
         val result = scala.io.Source.fromURL(url).mkString
-        println(result)
+        createFile(result)
     }
 
     def searchTopHeadlines(search: String, country: String, category: String, sources: String, page: String): Unit = {
-        val url = "https://newsapi.org/v2/everything" + 
+        val url = "https://newsapi.org/v2/top-headlines" + 
                   "?q=" + search +  
                   "&country=" + country +
                   "&category=" + category +
@@ -33,13 +40,32 @@ class apiConnect() {
                   "&page=" + page +
                   "&apiKey=0425fffcba054e00a1e722ffb03f7ff5"
         val result = scala.io.Source.fromURL(url).mkString
-        println(result)
+        
+        createFile(result)
     }
 
+    def createFile(result: String): Unit = {
+        val path = "hdfs://sandbox-hdp.hortonworks.com:8020/user/maria_dev/"
+        val filename = path + "Output.json"
+        println(s"Creating file $filename ...")
+        
+        val conf = new Configuration()
+        val fs = FileSystem.get(conf)
+        
+        // Check if file exists. If yes, delete it.
+        println("Checking if it already exists...")
+        val filepath = new Path( filename)
+        val isExisting = fs.exists(filepath)
+        if(isExisting) {
+            println("Yes it does exist. Deleting it...")
+            fs.delete(filepath, false)
+        }
 
-    def simpleApi(search: String, category: String): Unit = {
-    val url = "https://newsapi.org/v2/top-headlines?q=" + search + "&category=" + category + "&apiKey=0425fffcba054e00a1e722ffb03f7ff5"
-    val result = scala.io.Source.fromURL(url).mkString
-    println(result)
-  }
+        val output = fs.create(new Path(filename))  
+        val writer = new PrintWriter(output)
+        writer.write(result)
+        writer.close()
+        
+        println(s"Done creating file $filename ...\n")
+    }
 }
