ca30dc026da9d4a8d9555f3092462d93a8479596
Michael Hayes <michaelhayes22222@gmail.com>
2021-11-01 18:07:06 -0400

    Implemented Authentication and CreateUser Function

--- src/main/scala/helpers/hiveConnect.scala
+++ src/main/scala/helpers/hiveConnect.scala
@@ -10,30 +10,49 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.DriverManager;
+import scala.collection.mutable.ListBuffer
 
 
 import java.sql.Driver
 
 
 class hiveConnect(user: String, password: String) {
-    
-    def authenticate(): auth = {
-        val auth = new auth(true, true)
-        
+    val hg = new hiveGo()
+
+    def authenticate(): Auth = {
+        var auth = new Auth(false, false)
+        val column = "username, password, admin"
+        val table = "project1.notpasswords"
+        val logins = hg.select(column,table)
+        for (login <- logins){
+            val username = login._1
+            val pass = login._2
+            val admin = login._3
+            if(user == username && password == pass){
+                if(admin){
+                    auth = new Auth(true, true)
+                    println(s"Successfully logged in as $username\n")
+                }else{
+                    auth = new Auth(true, false)
+                    println(s"Successfully logged in as $username\n")
+                } 
+            }
+        }
         return auth
     }
 
     def login(): Unit = {
         var loop = true
         try {
-            if(authenticate().log){
-                if(authenticate().admin){
+            val auth: Auth = authenticate()
+            if(auth.log){
+                if(auth.admin){
                     admin()
                 }else{
                     user()
                 }
             }else{
-                println("Wrong Password or Username\n")
+                println("Wrong Password or Username!!\n")
             }
                 
         }catch {
@@ -46,24 +65,20 @@ class hiveConnect(user: String, password: String) {
     def admin(): Unit = {
         var loop = true
         try {
-            val hg = new hiveGo()
             var db: String = ""
             do {
                 println()
                 println("Please select an option")
-                println("1. Pull Data\n2. Show Databases\n3. Use Database\n4. Create Table\n5. Show Tables\n6. Describe Table\n7. Load Data into Table\n8. Test\n9. Log Out")
+                println("1. Pull & Load Data\n2. Show Databases\n3. Use Database\n4. Create Table\n5. Show Tables\n6. Describe Table\n7. Create User\n8. Log Out")
                 val option = scala.io.StdIn.readInt()
                 println()
                         
                 option match{
                     case 1 => {
                         hg.pullData(db)
-                        
-
                     }
                     case 2 => { 
-                        hg.showDB()
-                        
+                        hg.showDB()  
                     }
                     case 3 => {
                         var loop3: Boolean = true
@@ -80,29 +95,28 @@ class hiveConnect(user: String, password: String) {
                     }
                     case 4 => { 
                         hg.createTable(db)
-                        // NEEDS WORK
                     }
                     case 5 => {
                         hg.showTables(db)     
                     } 
                     case 6 => {
-                        hg.describeTable()            
+                        hg.describeTable(db)            
                     } 
                     case 7 => {
-                        hg.loadDataIntoTable()            
+                        hg.createUser(db)            
                     } 
                     case 8 => {
-                                
+                        loop = false 
+                        hg.closeConnection()             
                     }
                     case 9 => {
-                        loop = false 
-                        hg.closeConnection()           
+                                 
                     }   
                 }   
                 
             }while(loop)
         }catch{
-            case e: MatchError => println("Please pick a number between 1~4\n")
+            case e: MatchError => println("Please pick a number between 1~8\n")
             case e: NumberFormatException => println("\nPlease enter a number\n") 
         }
     }    
@@ -124,22 +138,31 @@ class hiveConnect(user: String, password: String) {
                                     
                         }
                         case 3 => {
-                            loop = false          
+                                     
                         }
                         case 4 => {
                                     
-                        }  
+                        }
+                        case 5 => {
+                                    
+                        }
+                        case 6 => {
+                                    
+                        }
+                        case 7 => {
+                            loop = false             
+                        }    
                     }
                 }while(loop)
 
             }catch{
-                case e: MatchError => println("Please pick a number between 1~4\n")
+                case e: MatchError => println("Please pick a number between 1~7\n")
                 case e: NumberFormatException => println("\nPlease enter a number\n") 
             }
     }
 }
 
-case class auth(log: Boolean, admin: Boolean)
+case class Auth(log: Boolean, admin: Boolean)
 
 class hiveGo(){
     var con: java.sql.Connection = null
@@ -191,17 +214,37 @@ class hiveGo(){
         println(dbList)
     }
 
+    def createUser(db: String): Unit = {
+         try {
+            println(s"Database: $db")
+            if(!db.isEmpty){
+                val tableName = "notpasswords"
+                val username = scala.io.StdIn.readLine("Enter Username: ") 
+                val password = scala.io.StdIn.readLine("Enter Password: ")
+                val admin = scala.io.StdIn.readLine("Enter Admin Status: ") 
+                println(s"Adding user to $tableName Table..")
+                stmt.execute(
+                    "insert into table " + db + "." + tableName + " select * from (select " + {'"'} + username + {'"'} + ", "+ {'"'} + password + {'"'}  +", " + {'"'} + admin + {'"'} + ")a"
+                );
+            }else{
+                println("Select a database to use!")
+            }
+        }catch {
+            case e: Throwable  => println("\nIncorrect syntax!!\nTry again\n")
+        }    
+    }
 
-    def select(column: String, table: String): Unit = {
-      println(s"Executing SELECT $column FROM $table..")
+    def select(column: String, table: String): ListBuffer[(String, String, Boolean)] = {
+      var logins = new ListBuffer[(String, String, Boolean)]()
+      println("\nAuthenticating...")
       var res = stmt.executeQuery("SELECT " + column + " FROM " + table)
       while (res.next()) {
-        println(s"${res.getString(1)}, ${res.getString(3)}")
-        //System.out.println(String.valueOf(res.getInt(1)) + "\t" + res.getString(2))
-        //System.out.println(res.getString(1))
+        //println(s"${res.getString(1)},${res.getString(2)}, ${res.getString(3)}")
+        logins += ((res.getString(1),res.getString(2), res.getString(3).toBoolean))
       }
+      logins
     }
-    //Create Exception handling for entering wrong column layout
+
     def createTable(db: String): Unit = {
         try {
             println(s"Database: $db")
@@ -209,11 +252,10 @@ class hiveGo(){
                 val tableName = scala.io.StdIn.readLine("Enter Table Name: ") 
                 val columns = scala.io.StdIn.readLine("Enter Column and Datatypes: ")
                 println(s"Dropping table $tableName..")
-                stmt.execute("drop table IF EXISTS " + tableName);
+                stmt.execute("drop table IF EXISTS " + db + "." + tableName);
                 println(s"Creating table $tableName..")
-                stmt.execute("use " + db)
                 stmt.execute(
-                    "create table " + tableName + " " + columns +" row format delimited  fields terminated by ','"
+                    "create table " + db + "." + tableName + " " + columns +" row format delimited  fields terminated by ','"
                 );
             }else{
                 println("Select a database to use!")
@@ -238,28 +280,16 @@ class hiveGo(){
         }
     }
     
-    def describeTable(): Unit = {
+    def describeTable(db: String): Unit = {
         val tableName = scala.io.StdIn.readLine("Enter Table Name: ") 
         // describe table
-        println(s"Describing table $tableName..")
+        println(s"Describing table $db.$tableName..")
         var sql = "describe " + tableName
         System.out.println("Running: " + sql)
         var res = stmt.executeQuery(sql)
         while (res.next()) {
             System.out.println(res.getString(1) + "\t" + res.getString(2))
         }
-    }
-
-    def loadDataIntoTable(): Unit = {
-        val tableName = scala.io.StdIn.readLine("Enter Table Name: ")
-        val filepath = "/tmp/a.txt";
-        // load data into table
-        // NOTE: filepath has to be local to the hive server
-        // NOTE: /tmp/a.txt is a comma separated file with two fields per line
-        // For E.g.; 1,"One"
-        var sql = "load data local inpath '" + filepath + "' into table " + tableName
-        System.out.println("Running: " + sql)
-        stmt.execute(sql);
     }
 
     def closeConnection(): Unit = {
@@ -275,33 +305,34 @@ class hiveGo(){
     }
 
     def pullData(db: String): Unit = {
-        val api = new apiConnect(db)
-        var loop = true
-        try {
-            do {
-                println("Please select an option")
-                println("1. Pull All News\n2. Pull Top Headlines\n3. Go Back")
-                val option = scala.io.StdIn.readInt()
-                println()
-                           
-                option match{
-                    case 1 => {
-                        api.searchEverything("game", "","" ,"","","", "1")
+        if(!db.isEmpty){
+            val api = new apiConnect(db)
+            var loop = true
+            try {
+                do {
+                    println("Please select an option")
+                    println("1. Pull All News\n2. Pull Top Headlines\n3. Go Back")
+                    val option = scala.io.StdIn.readInt()
+                    println()
+                            
+                    option match{
+                        case 1 => {
+                            api.searchEverything("game", "","" ,"","","", "1")
+                        }
+                        case 2 => {
+                            api.searchTopHeadlines("game","","technology","","1")
+                        }
+                        case 3 => {
+                            loop = false
+                        }
                     }
-                    case 2 => {
-                        api.searchTopHeadlines("game","","technology","","1")
-                    }
-                    case 3 => {
-                        loop = false
-                    }
-                }
-
-
-
-            }while(loop)
-        }catch{
-            case e: MatchError => println("Please pick a number between 1~2\n")
-            case e: NumberFormatException => println("\nPlease enter a number\n") 
+                }while(loop)
+            }catch{
+                case e: MatchError => println("Please pick a number between 1~3\n")
+                case e: NumberFormatException => println("\nPlease enter a number\n") 
+            }
+        }else{
+            println("Please use a database first!!")
         }
     }
 }
